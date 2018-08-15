8bb4d82fdc4ec3e3c001d725f13670ef85ce6a90
cvonwilczur <cvonwilczur@gmail.com>
2018-08-15 13:07:16 -0700

    basic structure set up for two routes

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('This will be a landing page soon.');
+  res.render('landing');
 })
 
 app.get('/groups', (req, res) => {
-  res.send('Similarly, this will be a group page soon.');
+  const groups = [
+    {name: 'Tempe Dungeons', image: 'https://cdn.vox-cdn.com/thumbor/z7RBQFbTwOFqy31s-OmA-9G6K4U=/0x0:5026x3351/1200x800/filters:focal(2111x1274:2915x2078)/cdn.vox-cdn.com/uploads/chorus_image/image/57635507/692647154.jpg.0.jpg'},
+    {name: 'Phoenix Dragons', image: 'http://castlesandcooks.com/wp-content/uploads/2011/03/Dming-600x337.jpg'},
+    {name: 'Chandler Pathfinders', image: 'https://i0.wp.com/www.geeksaresexy.net/wp-content/uploads/2014/11/danddnoob.png?resize=600%2C326'}
+  ]
+
+  res.render('groups', {groups: groups});
 })
 
 app.listen(3000, () => {
