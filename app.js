75f5b2dba65e06b55a53da9371a9e3dd807106b9
cvonwilczur <cvonwilczur@gmail.com>
2018-08-16 08:49:52 -0700

    added details section, connected to mongodb database with mongoose

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost/yelpdnd')
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-const groups = [
-  {name: 'Tempe Dungeons', image: 'https://cdn.vox-cdn.com/thumbor/z7RBQFbTwOFqy31s-OmA-9G6K4U=/0x0:5026x3351/1200x800/filters:focal(2111x1274:2915x2078)/cdn.vox-cdn.com/uploads/chorus_image/image/57635507/692647154.jpg.0.jpg'},
-  {name: 'Phoenix Dragons', image: 'http://castlesandcooks.com/wp-content/uploads/2011/03/Dming-600x337.jpg'},
-  {name: 'Chandler Pathfinders', image: 'https://i0.wp.com/www.geeksaresexy.net/wp-content/uploads/2014/11/danddnoob.png?resize=600%2C326'}
-]
+
+//SCHEMA SETUP
+const groupSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+const Group = mongoose.model('Group', groupSchema);
+
+// Group.create(  {name: 'Phoenix Dragons', image: 'http://castlesandcooks.com/wp-content/uploads/2011/03/Dming-600x337.jpg', description: 'This groups is running Curse of Strahd currently and is around level 5.'},
+//             (err, group) => {
+//               if(err){
+//                 console.log(err);
+//               } else {
+//                 console.log('Newly created group');
+//                 console.log(group);
+//               }
+//             });
 
 app.get('/', (req, res) => {
   res.render('landing');
 })
 
 app.get('/groups', (req, res) => {
-  res.render('groups', {groups: groups});
+  Group.find({}, (err, allGroups) => {
+    if(err){
+      console.log(err);
+    } else {
+      res.render('index', {groups: allGroups});
+    }
+  })
 })
 
 app.get('/groups/new', (req, res) => {
@@ -27,11 +49,25 @@ app.get('/groups/new', (req, res) => {
 app.post('/groups', (req, res) => {
   let name = req.body.name;
   let image = req.body.image;
-  let newGroup = {name: name, image: image};
-  groups.push(newGroup);
-  //get data from form and add to groups array
-  //also want to redirect back to groups page
-  res.redirect('/groups');
+  let desc = req.body.description;
+  let newGroup = {name: name, image: image, description: desc};
+  Group.create(newGroup, (err, newlyCreated) => {
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect('/groups');
+    }
+  })
+})
+
+app.get('/groups/:id', (req, res) => {
+  Group.findById(req.params.id, (err, foundGroup) => {
+    if(err){
+      console.log(err);
+    } else {
+      res.render('show', {group: foundGroup});
+    }
+  })
 })
 
 app.listen(3000, () => {
